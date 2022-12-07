949464af8f8c28dadf4a18e59c758f4cfb012ab9
quentalt <quentin.altieri@gmail.com>
2022-12-07 19:30:29 +0100

    Initial commit

---
+++ Pacman/Pacman.java
@@ -0,0 +1,21 @@
+package Pacman;
+
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+		
+	public Pacman() {
+		add(new Model());
+	}
+	
+	public static void main(String[] args) {
+		
+	Pacman pac = new Pacman();
+	pac.setVisible(true);
+	pac.setTitle("Pacman");
+	pac.setSize(400,500);
+	pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	pac.setLocationRelativeTo(null);
+	}
+
+}
